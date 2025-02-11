864e0f8f6ff39250ffba41f79bc45485570f3a23
DESKTOP-OO7EOJK\user1 <kh_user>
2025-02-11 09:19:32 +0900

    no message

--- app98/src/main/java/com/kh/app98/TestController.java
+++ app98/src/main/java/com/kh/app98/TestController.java
@@ -1,5 +1,6 @@
 package com.kh.app98;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/")
-    public String m01(){
+    public String m01(HttpServletRequest req){
+        // String v = (String) req.getAttribute("food");
+        // System.out.println("v = " + v);
         return "<h1>zzzzzzzzzzzz</h1>";
     }
 }
